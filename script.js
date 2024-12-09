4818d2ddd38419cee937ba822ec4432943a4fd59
Enijeshiku Eniola <eenijeshiku@gmail.com>
2024-12-09 04:43:19 +0100

    Test fix from branch

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerButton = document.getElementById('hamburgerButton');
-  const mobileMenu = document.getElementById('mobileMenu');
-
-  hamburgerButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-  });

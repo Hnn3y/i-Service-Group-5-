10cd71717b8cea7c8f57b51ee846b61026c46515
Enijeshiku Eniola <eenijeshiku@gmail.com>
2024-12-22 21:06:24 +0100

    New fix

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+const hamburgerButton = document.getElementById('hamburgerButton');
+  const mobileMenu = document.getElementById('mobileMenu');
+
+  hamburgerButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
